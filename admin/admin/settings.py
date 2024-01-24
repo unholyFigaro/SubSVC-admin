9f83db0ade5f2dc2dd72562af54470aa6a97d005
unholyFigaro <71033284+unholyFigaro@users.noreply.github.com>
2024-01-24 10:20:43 +0300

    Update settings.py

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'ec2-13-53-40-213.eu-north-1.compute.amazonaws.com'
+    'ec2-13-53-40-213.eu-north-1.compute.amazonaws.com',
+    'ec2-13-51-201-246.eu-north-1.compute.amazonaws.com'
     ]
 
 
